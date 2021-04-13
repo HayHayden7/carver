1ab465b1f24cf357b438bdd7a1dc5b11bb8e9b70
HayHayden7 <Hayden@DESKTOP-TS49VVC>
2021-04-13 10:22:45 -0700

    Andesite

---
+++ src/main/java/net/mcreator/carver/block/AndesiteTwistedBlock.java
@@ -0,0 +1,101 @@
+
+package net.mcreator.carver.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.carver.itemgroup.CarverCreativeTabItemGroup;
+import net.mcreator.carver.CarverModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CarverModElements.ModElement.Tag
+public class AndesiteTwistedBlock extends CarverModElements.ModElement {
+	@ObjectHolder("carver:andesite_twisted")
+	public static final Block block = null;
+	public AndesiteTwistedBlock(CarverModElements instance) {
+		super(instance, 222);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(CarverCreativeTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = DirectionalBlock.FACING;
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(2f, 30f).setLightLevel(s -> 0).harvestLevel(0)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.SOUTH));
+			setRegistryName("andesite_twisted");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Twisted"));
+		}
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		@Override
+		public BlockState rotate(BlockState state, Rotation rot) {
+			if (rot == Rotation.CLOCKWISE_90 || rot == Rotation.COUNTERCLOCKWISE_90) {
+				if ((Direction) state.get(FACING) == Direction.WEST || (Direction) state.get(FACING) == Direction.EAST) {
+					return state.with(FACING, Direction.UP);
+				} else if ((Direction) state.get(FACING) == Direction.UP || (Direction) state.get(FACING) == Direction.DOWN) {
+					return state.with(FACING, Direction.WEST);
+				}
+			}
+			return state;
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			Direction facing = context.getFace();
+			if (facing == Direction.WEST || facing == Direction.EAST)
+				facing = Direction.UP;
+			else if (facing == Direction.NORTH || facing == Direction.SOUTH)
+				facing = Direction.EAST;
+			else
+				facing = Direction.SOUTH;;
+			return this.getDefaultState().with(FACING, facing);
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
