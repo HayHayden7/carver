7e572378bba7743b9ad373d01cd3c73151318928
HayHayden7 <Hayden@DESKTOP-TS49VVC>
2021-04-13 08:59:29 -0700

    Diorite, lapis, oak planks, cobblestone.

---
+++ src/main/java/net/mcreator/carver/block/OakPlankBlindsBlock.java
@@ -0,0 +1,62 @@
+
+package net.mcreator.carver.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.carver.itemgroup.CarverCreativeTabItemGroup;
+import net.mcreator.carver.CarverModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CarverModElements.ModElement.Tag
+public class OakPlankBlindsBlock extends CarverModElements.ModElement {
+	@ObjectHolder("carver:oak_plank_blinds")
+	public static final Block block = null;
+	public OakPlankBlindsBlock(CarverModElements instance) {
+		super(instance, 85);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(CarverCreativeTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1f, 3f).setLightLevel(s -> 0));
+			setRegistryName("oak_plank_blinds");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Blinds"));
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/carver/itemgroup/CarverCreativeTabItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.carver.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.carver.CarverModElements;
+
+@CarverModElements.ModElement.Tag
+public class CarverCreativeTabItemGroup extends CarverModElements.ModElement {
+	public CarverCreativeTabItemGroup(CarverModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabcarver_creative_tab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(Blocks.STONECUTTER, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return true;
+			}
+		}.setBackgroundImageName("item_search.png");
+	}
+	public static ItemGroup tab;
+}
