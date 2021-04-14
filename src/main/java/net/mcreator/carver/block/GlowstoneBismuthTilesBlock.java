2c2dbba61ff6a83d84e1d57e5c3389ebf0a92c79
HayHayden7 <Hayden@DESKTOP-TS49VVC>
2021-04-13 17:29:43 -0700

    Glowstone (no recipes)

---
+++ src/main/java/net/mcreator/carver/block/GlowstoneBismuthTilesBlock.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.carver.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.carver.itemgroup.CarverCreativeTabItemGroup;
+import net.mcreator.carver.CarverModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CarverModElements.ModElement.Tag
+public class GlowstoneBismuthTilesBlock extends CarverModElements.ModElement {
+	@ObjectHolder("carver:glowstone_bismuth_tiles")
+	public static final Block block = null;
+	public GlowstoneBismuthTilesBlock(CarverModElements instance) {
+		super(instance, 600);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(CarverCreativeTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.GLASS).sound(SoundType.GLASS).hardnessAndResistance(0.3f, 0.3f).setLightLevel(s -> 15));
+			setRegistryName("glowstone_bismuth_tiles");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Bismuth Tiles"));
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(Items.GLOWSTONE_DUST, (int) (2)));
+		}
+	}
+}
