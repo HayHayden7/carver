c280ff1422d2d3a91b49ae6362368652c59e8186
HayHayden7 <Hayden@DESKTOP-TS49VVC>
2021-04-14 21:21:26 -0700

    New lanterns and pumpkins

---
+++ src/main/java/net/mcreator/carver/block/PumpkinChillBlock.java
@@ -0,0 +1,90 @@
+
+package net.mcreator.carver.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.carver.itemgroup.CarverCreativeTabItemGroup;
+import net.mcreator.carver.CarverModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CarverModElements.ModElement.Tag
+public class PumpkinChillBlock extends CarverModElements.ModElement {
+	@ObjectHolder("carver:pumpkin_chill")
+	public static final Block block = null;
+	public PumpkinChillBlock(CarverModElements instance) {
+		super(instance, 806);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(CarverCreativeTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+		public CustomBlock() {
+			super(Block.Properties.create(Material.GOURD).sound(SoundType.WOOD).hardnessAndResistance(1f, 1f).setLightLevel(s -> 0));
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+			setRegistryName("pumpkin_chill");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public void addInformation(ItemStack itemstack, IBlockReader world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Chill"));
+		}
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		public BlockState rotate(BlockState state, Rotation rot) {
+			return state.with(FACING, rot.rotate(state.get(FACING)));
+		}
+
+		public BlockState mirror(BlockState state, Mirror mirrorIn) {
+			return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			;
+			return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/carver/procedures/SoulLanternOvergrownHangingProcedureProcedure.java
@@ -0,0 +1,67 @@
+package net.mcreator.carver.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.carver.block.SoulLanternOvergrownHangingBlock;
+import net.mcreator.carver.block.SoulLanternOvergrownBlock;
+import net.mcreator.carver.CarverModElements;
+import net.mcreator.carver.CarverMod;
+
+import java.util.Map;
+
+@CarverModElements.ModElement.Tag
+public class SoulLanternOvergrownHangingProcedureProcedure extends CarverModElements.ModElement {
+	public SoulLanternOvergrownHangingProcedureProcedure(CarverModElements instance) {
+		super(instance, 788);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				CarverMod.LOGGER.warn("Failed to load dependency entity for procedure SoulLanternOvergrownHangingProcedure!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				CarverMod.LOGGER.warn("Failed to load dependency x for procedure SoulLanternOvergrownHangingProcedure!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				CarverMod.LOGGER.warn("Failed to load dependency y for procedure SoulLanternOvergrownHangingProcedure!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				CarverMod.LOGGER.warn("Failed to load dependency z for procedure SoulLanternOvergrownHangingProcedure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				CarverMod.LOGGER.warn("Failed to load dependency world for procedure SoulLanternOvergrownHangingProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((((entity.world.rayTraceBlocks(new RayTraceContext(entity.getEyePosition(1f),
+				entity.getEyePosition(1f).add(entity.getLook(1f).x * 5, entity.getLook(1f).y * 5, entity.getLook(1f).z * 5),
+				RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE, entity)).getFace()) == Direction.DOWN)
+				&& ((!(world.isAirBlock(new BlockPos((int) x, (int) (y + 1), (int) z))))
+						&& (world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z)).isSolid())))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), SoulLanternOvergrownHangingBlock.block.getDefaultState(), 3);
+		} else if ((((entity.world.rayTraceBlocks(new RayTraceContext(entity.getEyePosition(1f),
+				entity.getEyePosition(1f).add(entity.getLook(1f).x * 5, entity.getLook(1f).y * 5, entity.getLook(1f).z * 5),
+				RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE, entity)).getFace()) == Direction.UP)
+				&& ((!(world.isAirBlock(new BlockPos((int) x, (int) (y - 1), (int) z))))
+						&& (world.getBlockState(new BlockPos((int) x, (int) (y - 1), (int) z)).isSolid())))) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), SoulLanternOvergrownBlock.block.getDefaultState(), 3);
+		}
+	}
+}
